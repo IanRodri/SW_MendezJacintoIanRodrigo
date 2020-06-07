46295c1de72dcee28eece0adb45a03b00e56e8f9
IanRodri <rodkid02@gmail.com>
2020-06-07 18:01:47 -0500

    FEAT: Actividad 2

---
+++ Actividad2.php
@@ -0,0 +1,8 @@
+<?php
+  echo "Méndez Jacinto Ian Rodrigo";
+  echo "<br>";
+  echo "<ul>
+        <li>Hasta ahora no había entendido completamente el uso de la estructura for.</li>
+        <li>Y me agrada conocer el lenguaje php.</li>
+        </ul>";
+?>
